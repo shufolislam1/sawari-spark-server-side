0c3ab946026ef4c88468a9d140334a1316620317
shufol islam <shufolislam1255@gmail.com>
2022-05-23 11:45:54 +0600

    all data connected from mongo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
@@ -7,6 +8,34 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nh40c.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect()
+
+        const sparkCollection = client.db("sawariSpark").collection("tools")
+
+        app.get('/spark', async (req, res) => {
+            const query = {}
+            const cursor = sparkCollection.find(query);
+            const spark = await cursor.toArray();
+            res.send(spark)
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
